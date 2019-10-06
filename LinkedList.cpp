937674347d3e3a3d58f02d70f10826e81626b159
nidaa <nida.alk94@gmail.com>
2019-10-06 19:28:03 +0300

    Add new method InsetToList-head, test cases in main

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 #include "stdio.h"
 #include <string.h>
 
@@ -11,8 +11,35 @@ struct Node {
 };
 struct Node* head;
 
+
+struct Node* insertIntoList(int data_) {
+
+	if (head == NULL) {
+		struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+		temp->data = data_;
+		temp->next = head;
+
+		head = temp;
+	}
+	else {
+		struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+		temp->data = data_;
+		temp->next = head;
+
+		head = temp;
+	}
+	return head;
+}
+
+
+
 int main() {
 	
+	insertIntoList(1);
+	insertIntoList(5);
+	insertIntoList(6);
+	printf("%d", head->data);
+
 	return 0;
 
 }
