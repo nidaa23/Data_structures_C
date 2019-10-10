a07951544dca884b91287a48a00008ea6cd45404
nidaa <nida.alk94@gmail.com>
2019-10-10 15:04:47 +0300

    add class Node

---
+++ DoublyCircularLinkedList.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "stdio.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+
+struct Node {
+
+	int data;
+	struct Node* next;
+	struct Node* prev;
+
+};
+struct Node* head;
+struct Node *tail;
+int length = 0;
+
+
+
+
+int main() {
+
+
+}
