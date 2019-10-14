89551df8abbd23d557aa0850d44dcbe460c9bfd3
nidaa <nida.alk94@gmail.com>
2019-10-15 00:54:06 +0300

    Add new metthod createnewNode

--- DoublyCircularLinkedList.cpp
+++ DoublyCircularLinkedList.cpp
@@ -18,9 +18,19 @@ struct Node *tail;
 int length = 0;
 
 
+struct Node* CreatenewNode(int x) {
+		struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+		new_node->data = x;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		return new_node;
+	
 
+}
 
 int main() {
 
 
+
+	return 0;
 }
